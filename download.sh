c7544568eeaa85763d84ceec900c7960e53ca8cd
Ayumi Manome <ayumimanome@Ayumi-Fujisaki-Manomes-MacBook-Pro.local>
2025-06-04 10:32:50 -0400

    update - adding a Linux bash option

--- download.sh
+++ download.sh
@@ -3,7 +3,6 @@
 # set start and end dates
 date_start="2023-10-01"
 date_end="2023-11-02"
-
 #date_end="2025-06-01"
 date_now=$date_start
 
@@ -16,10 +15,12 @@ URL="https://apps.glerl.noaa.gov/thredds/fileServer/glsea_ice_nc/"
 while [[ $date_now != $date_end ]]; do
 
  # get year
- year=`date -j -f "%Y-%m-%d" $date_now "+%Y"`
+ year=`date -j -f "%Y-%m-%d" $date_now "+%Y"` # for Mac
+# year=`date -d $date_now  "+%Y"` # for Linux bash
 
  # get month
- month=`date -j -f "%Y-%m-%d" $date_now "+%m"`
+ month=`date -j -f "%Y-%m-%d" $date_now "+%m"` # for Mac
+# month=`date -d $date_now  "+%m"` # for Linux bash
 
  # if July - October, skip. Ice data files for these months are not provided.
  if [ "$month" -ge 6 ] && [ $month -le 10 ]; then
@@ -29,7 +30,8 @@ while [[ $date_now != $date_end ]]; do
  else
  
   # get julian day (days since Jan 1 in each year)
-  julian_day=`date -j -f "%Y-%m-%d" $date_now "+%j"`
+  julian_day=`date -j -f "%Y-%m-%d" $date_now "+%j"` # for Mac
+# julian_day=`date -d $date_now  "+%j"` # for Linux bash
 
   # full URL and file name to download
   fname=${URL}${year}"/"${month}"/"${year}"_"${julian_day}"_glsea_ice.nc"
@@ -42,8 +44,8 @@ while [[ $date_now != $date_end ]]; do
 
 
  # add a day
- date_now=`date -j -v +1d -f "%Y-%m-%d" $date_now +%Y-%m-%d`
-
+ date_now=`date -j -v +1d -f "%Y-%m-%d" $date_now +%Y-%m-%d` # for Mac
+#  date_now=`date -d "$date_now+1 days" +"%Y-%m-%d"` # for Linux bash
 
 done
 
