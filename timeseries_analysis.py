54eece7e6eeef7d2bc451af953609abe79b8cf80
Ayumi Manome <ayumimanome@Ayumi-Fujisaki-Manomes-MacBook-Pro.local>
2025-06-19 14:47:11 +0900

    loffset update

--- timeseries_analysis.py
+++ timeseries_analysis.py
@@ -9,8 +9,8 @@ df['date'] = pd.to_datetime(df['date'])
 df.set_index('date',inplace=True)
 
 # print header
-print(df.head())
-print(df.info())
+#print(df.head())
+#print(df.info())
 
 # create a figure
 fig, ax = plt.subplots()
@@ -20,7 +20,9 @@ fig, ax = plt.subplots()
 ax.plot_date(df.index, df[' mean_ice_coverage [%]'],'-',label="daily")
 
 # create monthly average
-monthly = df[' mean_ice_coverage [%]'].resample('MS', loffset=pd.Timedelta(15, 'd')).mean()
+monthly = df[' mean_ice_coverage [%]'].resample('MS',loffset=pd.Timedelta(15, 'd')).mean()
+# use below for pandas earlier than 1.1.0 
+#monthly = df[' mean_ice_coverage [%]'].resample('MS', loffset=pd.Timedelta(15, 'd')).mean()
 ax.plot_date(monthly.index, monthly,'*-',label="monthly")
 print(monthly)
 
@@ -30,7 +32,7 @@ annual_max_index = df.groupby(pd.Grouper(freq='Y'))[' mean_ice_coverage [%]'].id
 annual_max.index = annual_max_index
 
 ax.plot_date(annual_max_index, annual_max,'o',markersize=12,label="annual maximum")
-print(annual_max)
+#print(annual_max)
 ax.legend()
 
 fig.autofmt_xdate()
